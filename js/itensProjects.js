472192bed08b852263239d8352967d23e03073ea
Maisa Gabriela Rodrigues <maisagabirodrigues@gmail.com>
2025-04-05 21:15:30 -0300

    fix: js itens projects

---
+++ js/itensProjects.js
@@ -0,0 +1,45 @@
+// Dados dos projetos
+export const projetos = [
+    // {
+    //     titulo: "Projeto 1",
+    //     descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //     imagem: "img/projects/examples/1.jpg",
+    //     link: "#"
+    // },
+    // {
+    //     titulo: "Projeto 2",
+    //     descricao: "Proin gravida nibh vel velit auctor aliquet.",
+    //     imagem: "img/projects/examples/2.jpg",
+    //     link: "#"
+    // },
+    // {
+    //     titulo: "Projeto 3",
+    //     descricao: "Aenean sollicitudin, lorem quis bibendum auctor.",
+    //     imagem: "img/projects/examples/3.jpg",
+    //     link: "#"
+    // },
+    // {
+    //     titulo: "Projeto 4",
+    //     descricao: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    //     imagem: "img/projects/examples/1.jpg",
+    //     link: "#"
+    // },
+    // {
+    //     titulo: "Projeto 5",
+    //     descricao: "Proin gravida nibh vel velit auctor aliquet.",
+    //     imagem: "img/projects/examples/2.jpg",
+    //     link: "#"
+    // },
+    // {
+    //     titulo: "Projeto 6",
+    //     descricao: "Aenean sollicitudin, lorem quis bibendum auctor.",
+    //     imagem: "img/projects/examples/3.jpg",
+    //     link: "#"
+    // },
+    // {
+    //     titulo: "Projeto 7",
+    //     descricao: "Aenean sollicitudin, lorem quis bibendum auctor.",
+    //     imagem: "img/projects/examples/1.jpg",
+    //     link: "#"
+    // }
+];
